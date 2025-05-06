ab7ae07ed3f5e4a143dd397c07f43b15c47e333d
William Christian <wllchrst@outlook.com>
2025-05-06 14:54:57 +0700

    feat: additional env variable

--- helpers/env_helper.py
+++ helpers/env_helper.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-ENVS = ["GEMINI_API_KEY"]
+ENVS = ["GEMINI_API_KEY", "MODEL_PATH"]
 
 class EnvHelper:
     """Class for gathering and saving all env for the application """
@@ -29,5 +29,6 @@ class EnvHelper:
     
     def assign_env(self):
         self.GEMINI_API_KEY = self.envs[ENVS[0]]
+        self.MODEL_PATH = self.envs[ENVS[1]]
 
 env_helper = EnvHelper()
